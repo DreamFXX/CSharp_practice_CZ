60bf80d4b8aa10c2101dcfb7c2e62667aa79cf46
DreamFXX <filipek.husak@gmail.com>
2024-10-19 22:36:32 +0200

    Variables and writing - first project done.

--- IntroductionCSharp1/Program.cs
+++ IntroductionCSharp1/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace IntroductionCSharp1
 {
     internal class Program
+
+        // Deklarace a práce s proměnnými, jednotlivými typy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Proměnná je vždy nějakého typu
+            // Proměnná se nějak jmenuje
+            // Níže - celé číslo, slova a věty, znak, desetinné číslo, destinné číslo které se nebude užívat jako dlouhé tedy celé ale usekne se.. využívá se třeba ve hrách.
+            // Níže - boolean -> hodnota pravda / nepravda
+            int i = 152;
+            string s = "Hello, World!";
+            char c = 'm';
+            double d = 144.88454546464648441311255555659843164964614666316164161d; //Na konci se píše "d"
+            float f = 120.4f; // Na konci se píše velké F, float je vždy kratší kvuli jednodussi praci se pouziva misto doublu
+            bool b = true;
+
+
+
+            Console.WriteLine(s);
+
+            Console.ReadKey();
         }
     }
 }
--- KalkulackaConsole2/obj/Debug/net8.0/KalkulackaConsole2.AssemblyInfo.cs
+++ KalkulackaConsole2/obj/Debug/net8.0/KalkulackaConsole2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("IntroductionCSharp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("KalkulackaConsole2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ec62efd20a02a3227f04697016abf41a7d1b092")]
-[assembly: System.Reflection.AssemblyProductAttribute("IntroductionCSharp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("IntroductionCSharp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("KalkulackaConsole2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("KalkulackaConsole2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Vygenerované třídou WriteCodeFragment nástroje MSBuild
