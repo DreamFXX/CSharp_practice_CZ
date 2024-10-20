7c71dd49305619374954735bbf11dcd8d6dae3ad
DreamFXX <filipek.husak@gmail.com>
2024-10-20 04:43:49 +0200

    Ammended practice to-do quest for project 2.

--- KalkulackaConsole2/Program.cs
+++ KalkulackaConsole2/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+        // Vytvoř kalkulačku, která bude sčítat 2 čísla a pak je na konci vypíše sečtené.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
