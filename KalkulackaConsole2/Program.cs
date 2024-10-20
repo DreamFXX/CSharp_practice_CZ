f69c8ddaacd95f5db4d9b9ded0394dcd98e99738
DreamFXX <filipek.husak@gmail.com>
2024-10-20 14:17:21 +0200

    Started writing project 2 - Calculator

--- KalkulackaConsole2/Program.cs
+++ KalkulackaConsole2/Program.cs
@@ -3,9 +3,20 @@
     internal class Program
     {
         // Vytvoř kalkulačku, která bude sčítat 2 čísla a pak je na konci vypíše sečtené.
+        // Také se naučíme převádět číslo na textový řetězec.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Professional calculator - by DreamFXX written in CSharp");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Zadej první číslo které chceš sčítat:");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("Zadej druhé číslo které chceš sčítat:");
+            string s1 = Console.ReadLine();
+
+            Console.ReadKey();
         }
     }
 }
