4ec97b9785fe05c8d5dbd05448ce0d590520b392
DreamFXX <filipek.husak@gmail.com>
2024-10-23 16:13:45 +0200

    Finished Upgraded calculator with if / else cycles.
    Logic in C#

--- MnohonasobnePrirazeni3/Program.cs
+++ MnohonasobnePrirazeni3/Program.cs
@@ -6,18 +6,26 @@
         {
             // POUZE TEORIE!
             // Mnohonasobna deklarace.
-            bool b, b1, b2, b3;
+            ///bool b, b1, b2, b3;
 
             // Mnohonasobne přiřazení.
-            b = b1 = true;
-            b2 = b3 = false;
+            /// b = b1 = true;
+            /// b2 = b3 = false;
 
             // ZACATEK PROGRAMU.
             // Práce s podmínkami.
+            // bool b;
+            // b = false;
 
-            if (true)
+            // Prace s porovnavacími znaky
+            // <, >, =>, <=, ...
+
+            Console.WriteLine("V kódu definováno číslo, které je 5 >= 3 a pod ním je cyklus if.");
+            bool b = 5 >= 3;
+
+            if (b)
             {
-
+                Console.WriteLine("Pět je větší nebo rovno třem.");
             }
 
 
---
+++ UpgradedCalc4/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace UpgradedCalc4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in my calculator!");
+
+            int i1, i2;
+
+            Console.WriteLine("Zadejte první číslo, se kterým chcete pracovat: ");
+            i1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zadejte druhé číslo, se kterým chcete pracovat: ");
+            i2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Nyní zadejte operaci, kterou chcete provést -> + sčítání, - odčítání, * násobení, / dělení.");
+            string result = Console.ReadLine();
+
+            // V konfiguraci projektu je volba nullable == disabled.!
+
+            if (result == "+")
+            {
+                Console.WriteLine("Výsledek je: " + (i1 + i2));
+            }
+
+            // Else if - aby to nebylo zaměřeno pouze na všechny 4 pokusy.
+
+            else if (result == "-")
+            {
+                Console.WriteLine("Výsledek je: " + (i1 - i2));
+            }
+
+            else if (result == "*")
+            {
+                Console.WriteLine("Výsledek je: " + (i1 * i2));
+            }
+
+            else if (result == "/")
+            {
+                if (i2 != 0) // Negace - pokud není nula.
+                {
+                    Console.WriteLine("Výsledek je: " + (i1 / i2));
+                }
+
+                else
+                {
+                    Console.WriteLine("Dělitel nesmí být 0!");
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Nezadali jste ani jeden z požadovaných znaků.");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
